c90ab4000d15ad2b59d8556df5a9343de8b4e5f7
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2020-10-10 17:22:13 +0530

    left rotation of array of size by e elements

---
+++ rotate.py
@@ -0,0 +1,16 @@
+def left_rotate(arr,e,n):
+    for i in range(e):
+        temp = arr[0]
+        for j in range(n-1):
+            arr[j] = arr[j+1]
+            
+            arr[n-1] = temp
+             
+    for k in range(n):
+        print(arr[k])
+
+arr = [2,4,1,1,5,6]
+print(left_rotate(arr, 3, 6))
+
+
+
