b7ee720e6e6b2343fa3c66790ee7651279f6372f
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2020-10-13 12:29:20 +0530

    change

--- rotate.py
+++ rotate.py
@@ -3,13 +3,13 @@ def left_rotate(arr,e,n):
         temp = arr[0]
         for j in range(n-1):
             arr[j] = arr[j+1]
-            
-            arr[n-1] = temp
+            print(arr[j], end = " a ")
+        arr[n-1] = temp
              
     for k in range(n):
-        print(arr[k])
+        print(arr[k], "--")
 
-arr = [2,4,1,1,5,6]
+arr = [2,4,3,1,5,6]
 print(left_rotate(arr, 3, 6))
 
 
