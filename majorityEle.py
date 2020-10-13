d41c661d4f4613d9aab51a2cb64357138b4317b6
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2020-10-13 12:09:59 +0530

    Find majority element in a list

---
+++ majorityEle.py
@@ -0,0 +1,21 @@
+def majority_ele(arr):
+
+
+    max_count = 0
+    index = -1
+    for i in range(len(arr)):
+        count = 0
+        for j in range(len(arr)):
+            if arr[i] == arr[j]:
+                count += 1
+        if count > max_count:
+            max_count = count
+            index = i
+    if len(arr) // 2:
+        print(arr[index])
+    else:
+        print("No Majority Ele")
+
+arr = [2, 2, 5, 2, 3, 6]
+
+print(majority_ele(arr))
