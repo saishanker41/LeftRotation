dd1effff157cce2c656575be81f072ce9c88b3f1
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2020-10-13 12:24:51 +0530

    Finding missing number

--- missing_num.py
+++ missing_num.py
@@ -11,7 +11,7 @@ def missing_num(arr,n):
     sum_of_Elements = n *(n+1)//2
     return sum_of_Elements - sum_of(arr)
 
-arr = [1,3,4,5,6]
+arr = [1,2,3,4,5,6,8]
 n = len(arr) + 1
 print(missing_num(arr,n))    
 
